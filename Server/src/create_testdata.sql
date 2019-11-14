6a73656e4577b8d55a4ec47b60d8914ff288d697
heleneyj <heleneyj@stud.ntnu.no>
2019-11-14 14:19:52 +0100

    Trying to set CI

--- Server/src/create_testdata.sql
+++ Server/src/create_testdata.sql
@@ -1,11 +1,11 @@
-insert into annonse (title, picturePath, pictureAlt, pictureCapt, text, author, category, importance) values
-    ('tittel 1', 'https://images2.sw-cdn.net/product/picture/710x528_1129409_126266_1556338961.jpg',  'bildebeskrivese','bildetekst',
+insert into annonse (id, title, picturePath, pictureAlt, pictureCapt, text, date, author, category, importance) values
+    (1,'tittel 1', 'https://images2.sw-cdn.net/product/picture/710x528_1129409_126266_1556338961.jpg',  'bildebeskrivese','bildetekst', new Date("2017-01-26"),
     'tekst til nr 1', 'Helene Jonson', 'D&D',1),
-    ('tittel 2', 'https://i.pinimg.com/originals/a4/f2/a6/a4f2a6b43bf1fb4d1a8bfd9ce4eb446f.jpg',  'bildebeskrivese','bildetekst',
+    (2,'tittel 2', 'https://i.pinimg.com/originals/a4/f2/a6/a4f2a6b43bf1fb4d1a8bfd9ce4eb446f.jpg',  'bildebeskrivese','bildetekst', new Date("2017-01-26"),
     'tekst til nr 2', 'Bunde', 'D&D',2),
-    ('tittel 3', 'https://upload.wikimedia.org/wikipedia/en/a/af/Star_Wars_The_Rise_of_Skywalker_poster.jpg',  'bildebeskrivese','bildetekst',
+    (3,'tittel 3', 'https://upload.wikimedia.org/wikipedia/en/a/af/Star_Wars_The_Rise_of_Skywalker_poster.jpg',  'bildebeskrivese','bildetekst', new Date("2017-01-26"),
     'tekst til nr 3', 'Helene Jonson', 'Movies',1),
-    ('tittel 4', 'https://images-na.ssl-images-amazon.com/images/I/91UnxEI-Z5L.jpg',  'bildebeskrivese','bildetekst',
+    (4,'tittel 4', 'https://images-na.ssl-images-amazon.com/images/I/91UnxEI-Z5L.jpg',  'bildebeskrivese','bildetekst', new Date("2017-01-26"),
     'tekst til nr 4', 'Kari Normann', 'Books',1);
 
 insert into comments (articleId, username, text) values
