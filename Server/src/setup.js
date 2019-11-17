76e7a378b45e6c03f1d18c66d77bfb108b856b73
heleneyj <heleneyj@stud.ntnu.no>
2019-11-17 16:04:04 +0100

    Testing CI connection

--- Server/src/setup.js
+++ Server/src/setup.js
@@ -13,10 +13,10 @@ var pool = mysql.createPool({
 
 module.exports = async () => {
 
-  global.__MONGOD__ = beforeAll(done => {
+  global.__MONGOD__ =
     runsqlfile("src/create_tables.sql", pool, () => {
-      runsqlfile("src/create_testdata.sql", pool, done);
+      runsqlfile("src/create_testdata.sql", pool);
     })
-  });
 };
+
 
