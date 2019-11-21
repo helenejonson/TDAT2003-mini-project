918de6fb678025d5485cb49148c8a7cc0aad5387
heleneyj <heleneyj@stud.ntnu.no>
2019-11-21 12:37:45 +0100

    Testing CI connection

--- Server/src/serverTest/setup.js
+++ Server/src/serverTest/setup.js
@@ -1,8 +1,8 @@
 // @flow
 var mysql = require("mysql");
 const runsqlfile = require("./runsqlfile");
-require("babel-core/register");
-require("babel-polyfill");
+//require("babel-core/register");
+//require("babel-polyfill");
 
 var pool = mysql.createPool({
   connectionLimit: 1,
