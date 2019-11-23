27f137947eb73c99b94a4257daf8ea0f23e8a400
heleneyj <heleneyj@stud.ntnu.no>
2019-11-23 17:11:27 +0100

    trying button test

--- Klient/test/buttonTest.js
+++ Klient/test/buttonTest.js
@@ -2,13 +2,11 @@
 
 import * as React from 'react';
 import { Component } from 'react-simplified';
-
-
 import { AdvSearch } from '../src/search';
 import { shallow, mount } from 'enzyme';
 
 describe('Button test', () => {
-  const wrapper = shallow(<AdvSearch />);
+  const wrapper = shallow(<AdvSearch word="me" />);
 
   it('clicking button', () => {
     wrapper.find('button.btn').simulate('click');
